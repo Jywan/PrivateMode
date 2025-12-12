791f0424e971d39a14133c4d66c0e5ebad3a6493
Jywan <apdl815@naver.com>
2025-12-12 11:47:06 +0900

    Initial Commit

---
+++ PrivateMode/PrivateModeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrivateModeApp.swift
+//  PrivateMode
+//
+//  Created by 장영완 on 12/12/25.
+//
+
+import SwiftUI
+
+@main
+struct PrivateModeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
